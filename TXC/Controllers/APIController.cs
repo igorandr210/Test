0083f98d2bcfa64a2d2c3f94dcc88bbf1bbbb2f3
igorandr210 <33333613+igorandr210@users.noreply.github.com>
2018-08-02 20:17:17 +0300

    123

---
+++ TXC/Controllers/APIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nethereum.Web3;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TXC.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/API")]
+    public class APIController : Controller
+    {
+        [HttpPost]
+        public string createPayment(int orderId,ulong value)
+        {
+            return "s";
+        }
+    }
+}
